5354514be8a524235aaf64af65e8f1af1a966530
jerrysu5379 <jerrysu@fds.moe>
2024-12-02 10:33:20 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
       "/api": {
         target: "https://food-delivery-7660d10f4925.herokuapp.com",
